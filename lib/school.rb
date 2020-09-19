ef026670cf406e83fd33fb90f8d0b6f782712fcb
MJ Johnson <mjohnson4685@gmail.com>
2020-09-19 02:25:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,25 @@
 class School
- attr_accessor :roster
+  attr_accessor :roster
 
- def initialize(names)
-   @roster = {}
- end
+  def initialize(names)
+    @roster = {}
+  end
 
- def add_student(name, grade)
-   @roster[grade] ||= []
-   @roster[grade] << name
- end
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
 
- def grade(number)
-   @roster[number]
- end
+  def grade(number)
+    @roster[number]
+  end
 
- def sort
-   hash ={}
-   @roster.keys.sort.each do |number|
+  def sort
+     hash ={}
+     @roster.keys.sort.each do |number|
      hash[number]
-   @roster.sort
- end
+     @roster.sort
+  end
 
 end
  # code here!
