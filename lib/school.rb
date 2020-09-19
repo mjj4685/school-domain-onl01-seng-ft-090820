2c3bf42122cb1f1d88e4188d6adfce2c42921067
MJ Johnson <mjohnson4685@gmail.com>
2020-09-19 01:45:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,16 @@
 class School
+  attr_accessor :author, :page_count
+  attr_reader :name, :genre
 
+  def initialize(name)
+    @name = name
+  end
 
+  def roster
+    roster = {}
+  end
+  
+    
 
 end
  # code here!
