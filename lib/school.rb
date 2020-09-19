7e71dfe94c589830cde8de4a6c126f382b44343b
MJ Johnson <mjohnson4685@gmail.com>
2020-09-19 02:45:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster.detect do |x, y|
+      if x == grade
+        return y
+      end
+    end
+  end
+
+  def sort
+    roster_hash = {}
+    roster.each do |grade, students|
+    roster_hash[grade] = students.sort
+  end
+
+    roster_hash
+  end
+
+end
